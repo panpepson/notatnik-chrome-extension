427cbf50112b95f676d98f93b17c719d21fa384b
Piotr Trochymiak <Piotr@Trochymiak.net>
2020-12-10 13:07:55 +0100

    clear code

--- main.js
+++ main.js
@@ -2,12 +2,7 @@ window.onload = e => {
   let getTextFromTextArea = document.getElementById("Text");
   getTextFromTextArea.value = localStorage.getItem("pepNota");
 
-  // getTextFromTextArea.onkeyup = () => {
-  //   localStorage.setItem("pepNota", getTextFromTextArea.value);
-  // };
-
   getTextFromTextArea.oninput = () => {
-    console.log("1");
     localStorage.setItem("pepNota", getTextFromTextArea.value);
   };
 };
