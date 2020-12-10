4c8176cb421016478ad7c208dcf96270b8f6575a
Piotr Trochymiak <Piotr@Trochymiak.net>
2020-12-10 12:56:53 +0100

    change event onkeyup at oninput

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 window.onload = e => {
   let getTextFromTextArea = document.getElementById("Text");
   getTextFromTextArea.value = localStorage.getItem("pepNota");
-  getTextFromTextArea.onkeyup = () => {
+
+  // getTextFromTextArea.onkeyup = () => {
+  //   localStorage.setItem("pepNota", getTextFromTextArea.value);
+  // };
+
+  getTextFromTextArea.oninput = () => {
+    console.log("1");
     localStorage.setItem("pepNota", getTextFromTextArea.value);
   };
 };
